33625b82fef4f568ab48d4a2a8bc7d013721eee8
Eduardo Hermes Becker <eduardohermesb@gmail.com>
2024-10-08 16:53:54 -0300

    Inicialização

---
+++ src/Class/Palestra.java
@@ -0,0 +1,8 @@
+package Class;
+
+import java.util.List;
+
+public class Palestra extends Evento {
+    int duracao;
+    List<String> temas;
+}
---
+++ src/Class/Participante.java
@@ -0,0 +1,7 @@
+package Class;
+
+public class Participante {
+    String nome;
+    String email;
+    int tipo; //normal(1)  VIP(2)
+}
