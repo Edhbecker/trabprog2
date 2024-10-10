2b5bae65a06427152a6ebe04ff7f19e44c505bfe
Eduardo Hermes Becker <eduardohermesb@gmail.com>
2024-10-10 20:03:40 -0300

    Menu - Alteração no App.java e criação de classes

--- src/App.java
+++ src/App.java
@@ -1,8 +1,30 @@
 //import java.util.List;
 //import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+        Scanner s = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0){
+        
+            System.out.println("\nMenu");
+            System.out.println("1 - Cadastrar Evento");
+            System.out.println("2 - Cadastrar Participante");
+            System.out.println("3 - Gerenciar Reservas");
+            System.out.println("4 - Visualizar Relatório");
+            System.out.println("0 - Sair\n");
+            System.out.print("Digite o número da opção desejada: ");
+            opcao = s.nextInt();
+
+            System.out.println();
+            
+            }
+        s.close();
+        System.out.println("\nFinalizado\n");
+        }
+        
+        
 }
